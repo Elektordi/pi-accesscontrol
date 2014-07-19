b73ab223fb1e4b0eb59aa24fe63ba7a707d2357c
Elektordi <elektordi@elektordi.net>
2014-07-20 01:41:41 +0200

    Petite erreur de copier coller

--- src/backend/app/Config/core.php
+++ src/backend/app/Config/core.php
@@ -100,9 +100,7 @@
  * the plugin assets into you app's webroot directory. This is
  * recommended even when you are using mod_rewrite. Handling static
  * assets through the Dispatcher is incredibly inefficient and
- * included primarily as a development convenience - andConfigure::write('Cookie.SecurityKey', 'vKh5SYhHDzZ99cA1m19SDgwbNfgpLBUM8ZtVAf06pLiEp2gTwT31KEhrWbXnMZ5w');
-
-define('DATE_MYSQL', 'Y-m-d H:i:s');
+ * included primarily as a development convenience - and
  * thus not recommended for production applications.
  */
 	//Configure::write('App.baseUrl', env('SCRIPT_NAME'));
